ae002958b69a8960581947b0ea30e33b7ff26abf
eyesseekreality0 <eyesseekreality0@gmail.com>
2025-08-04 23:33:56 -0700

    Updated bun.lockb

--- src/data/games.ts
+++ src/data/games.ts
@@ -9,307 +9,307 @@ export const games: Game[] = [
   {
     id: "megaspinsweeps",
     name: "Mega Spin Sweeps",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/mega-spin-sweeps-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/85307f95.jpg?v=0c91e9dc",
     gameUrl: "http://www.megaspinsweeps.com/index.html"
   },
   {
     id: "vblink777",
     name: "VBLink777",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/vblink777-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/753a32c3.jpg?v=0c91e9dc",
     gameUrl: "https://www.vblink777.club/"
   },
   {
     id: "goldentreasure",
     name: "Golden Treasure",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/golden-treasure-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/7c9b03e5.jpg?v=0c91e9dc",
     gameUrl: "https://www.goldentreasure.mobi/"
   },
   {
     id: "orionstars",
     name: "Orion Stars",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/orion-stars-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/417aedb1.png?v=0c91e9dc",
     gameUrl: "http://start.orionstars.vip:8580/index.html"
   },
   {
     id: "firekirin",
     name: "Fire Kirin",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/fire-kirin-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/189aadee.jpg?v=0c91e9dc",
     gameUrl: "http://start.firekirin.xyz:8580/index.html"
   },
   {
     id: "rivermonster",
     name: "River Monster",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/river-monster-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/253c9f08.jpg?v=0c91e9dc",
     gameUrl: "https://rm777.net/"
   },
   {
     id: "riversweeps",
     name: "Riversweeps",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/riversweeps-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/597c1510.jpg?v=0c91e9dc",
     gameUrl: "https://bet777.eu/"
   },
   {
     id: "fortune2go",
     name: "Fortune 2 Go",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/fortune-2-go-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/d1498abc.jpg?v=0c91e9dc",
     gameUrl: "https://www.fortune2go20.com/mobile/Login/index.html"
   },
   {
     id: "goldendragon",
     name: "Golden Dragon",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/golden-dragon-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/de5f04bc.jpg?v=0c91e9dc",
     gameUrl: "https://playgd.mobi/SSLobby/3733.0/web-mobile/index.html"
   },
   {
     id: "bludragon",
     name: "Blu Dragon",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/blue-dragon-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/faeeb77b.png?v=0c91e9dc",
     gameUrl: "http://app.bluedragon777.com/"
   },
   {
     id: "vegasx",
     name: "VEGAS X",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/vegas-x-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/1e472144.jpg?v=0c91e9dc",
     gameUrl: "https://vegas-x.org/"
   },
   {
     id: "ultrapanda",
     name: "ULTRAPANDA",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/ultra-panda-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/ad5dd9c6.jpg?v=0c91e9dc",
     gameUrl: "https://www.ultrapanda.mobi/"
   },
   {
     id: "milkyways",
     name: "MILKY WAYS",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/milky-ways-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/2bc0a981.jpg?v=0c91e9dc",
     gameUrl: "https://milkywayapp.xyz/"
   },
   {
     id: "luckypenny",
     name: "LUCKY PENNY",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/lucky-penny-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/9b984f9c.jpg?v=0c91e9dc",
     gameUrl: "http://luckypenny.xyz:8580/index.html"
   },
   {
     id: "gametime",
     name: "Gametime",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/gametime-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/85307f95.jpg?v=0c91e9dc", // Using fallback
     gameUrl: "http://game-time.vip:8580/index.html"
   },
   {
     id: "goldstar",
     name: "Gold Star",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/gold-star-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/74eb627d.jpg?v=0c91e9dc",
     gameUrl: "https://goldstar.games/"
   },
   {
     id: "100plus",
     name: "100 Plus",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/100-plus-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/e00a2881.jpg?v=0c91e9dc",
     gameUrl: "https://99.100plus.me/lobby/1684487595/index.html?agreement=1&/player/release/"
   },
   {
     id: "gamevault",
     name: "Gamevault",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/gamevault-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/050dac4a.jpg?v=0c91e9dc",
     gameUrl: "https://download.gamevault999.com/"
   },
   {
     id: "galaxyworld",
     name: "Galaxy World",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/galaxy-world-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/1f44c7e1.png?v=0c91e9dc",
     gameUrl: "https://m.galaxyworld999.com/"
   },
   {
     id: "magiccity",
     name: "Magic City",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/magic-city-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/71d5fc8f.jpg?v=0c91e9dc",
     gameUrl: "https://www.magiccity777.com/SSLobby/3657.0/web-mobile/index.html"
   },
   {
     id: "highstake",
     name: "High Stake Sweeps",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/high-stake-sweeps-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/e1d3a0f7.jpg?v=0c91e9dc",
     gameUrl: "https://dl.highstakesweeps.com/"
   },
   {
     id: "sincity",
     name: "Sin City",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/sin-city-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/98333b44.jpg?v=0c91e9dc",
     gameUrl: "https://sincitysweeps.net/"
   },
   {
     id: "vegassweeps",
     name: "Vegas Sweeps",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/vegas-sweeps-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/bd38d36f.jpg?v=0c91e9dc",
     gameUrl: "https://m.lasvegassweeps.com/"
   },
   {
     id: "ignite",
     name: "IGNITE",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/ignite-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/3603f518.png?v=0c91e9dc",
     gameUrl: "https://casinoignite.vip/"
   },
   {
     id: "cashfrenzy",
     name: "Cashapp Frenzy",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/cashapp-frenzy-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/a67374ce.jpg?v=0c91e9dc",
     gameUrl: "https://www.cashfrenzy777.com/"
   },
   {
     id: "acebook",
     name: "ACEBOOK",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/acebook-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/09e14951.jpg?v=0c91e9dc",
     gameUrl: "https://www.playacebook.mobi/"
   },
   {
     id: "bluedragon2",
     name: "Blue Dragon 2",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/blue-dragon-2-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/637eb5ee.png?v=0c91e9dc",
     gameUrl: "http://app.getbluedragon.com/"
   },
   {
     id: "juwa777",
     name: "Juwa777",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/juwa777-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/bebb5d9e.jpg?v=0c91e9dc",
     gameUrl: "https://dl.juwa777.com/"
   },
   {
     id: "pandamaster",
     name: "Panda Master",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/panda-master-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/0b6843e0.jpg?v=0c91e9dc",
     gameUrl: "https://pandamaster.vip:8888/index.html"
   },
   {
     id: "fishglory",
     name: "Fish Glory",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/fish-glory-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/346fd5a6.jpg?v=0c91e9dc",
     gameUrl: "https://www.fishglory.games/"
   },
   {
     id: "vegasroll",
     name: "Vegas Roll",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/vegas-roll-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/7867671f.jpg?v=0c91e9dc",
     gameUrl: "http://www.vegas-roll.com/m"
   },
   {
     id: "mrallinone",
     name: "Mr All In One",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/mr-all-in-one-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/d6f9845f.jpg?v=0c91e9dc",
     gameUrl: "https://www.mrallinone777.com/m"
   },
   {
     id: "orionpower",
     name: "ORION Power",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/orion-power-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/6c415d9a.jpg?v=0c91e9dc",
     gameUrl: "http://product.orionpower.games/v1001/"
   },
   {
     id: "quakegame",
     name: "Quake Game",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/quake-game-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/0c63b506.jpg?v=0c91e9dc",
     gameUrl: "https://www.quakegame.net/"
   },
   {
     id: "vegasluck777",
     name: "Vegas Luck 777",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/vegas-luck-777-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/961ffa7e.jpg?v=0c91e9dc",
     gameUrl: "https://start.vegasluck777.com/"
   },
   {
     id: "noble777",
     name: "Noble 777",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/noble-777-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/21cf31e9.jpg?v=0c91e9dc",
     gameUrl: "https://www.noble777.com/m"
   },
   {
     id: "cashmachine777",
     name: "Cash Machine 777",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/cash-machine-777-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/d68e85e8.jpg?v=0c91e9dc",
     gameUrl: "https://www.cashmachine777.com/m"
   },
   {
     id: "gameroom777",
     name: "Game Room 777",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/game-room-777-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/92bca02e.jpg?v=0c91e9dc",
     gameUrl: "https://www.gameroom777.com/m"
   },
   {
     id: "luckystars",
     name: "Lucky Stars",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/lucky-stars-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/990e95bf.jpg?v=0c91e9dc",
     gameUrl: "http://www.luckystars.games/m"
   },
   {
     id: "slots88888",
     name: "Slots 88888",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/slots-88888-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/035f8253.jpg?v=0c91e9dc",
     gameUrl: "https://www.slots88888.com/m"
   },
   {
     id: "winstar99999",
     name: "Win Star 99999",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/win-star-99999-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/b48ed188.jpg?v=0c91e9dc",
     gameUrl: "http://server.winstar99999.com:8009/m"
   },
   {
     id: "mafia77777",
     name: "Mafia 77777",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/mafia-77777-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/a3830c1f.jpg?v=0c91e9dc",
     gameUrl: "http://product.mafia77777.com/v1003/"
   },
   {
     id: "yolo777",
     name: "YOLO 777",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/yolo-777-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/e1b31467.jpg?v=0c91e9dc",
     gameUrl: "https://yolo777.game/"
   },
   {
     id: "fpc",
     name: "FPC Mobile",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/fpc-mobile-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/2eb63b4e.png?v=0c91e9dc",
     gameUrl: "https://fpc-mob.com/AD/index.html"
   },
   {
     id: "winnersclub777",
     name: "Winners Club 777",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/winners-club-777-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/a9e0fca0.jpg?v=0c91e9dc",
     gameUrl: "https://www.winnersclub777.com/"
   },
   {
     id: "legendfire",
     name: "Legend Fire",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/legend-fire-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/8b67f370.jpg?v=0c91e9dc",
     gameUrl: "https://www.legendfire.xyz/"
   },
   {
     id: "firelinkplus",
     name: "Fire Link Plus",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/fire-link-plus-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/f592d30c.jpg?v=0c91e9dc",
     gameUrl: "https://firelinkplus.com/"
   },
   {
     id: "blackmamba",
     name: "Black Mamba",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/black-mamba-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/b99b4435.jpg?v=0c91e9dc",
     gameUrl: "https://blackmamba.mobi/"
   },
   {
     id: "playorca",
     name: "Play Orca",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/play-orca-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/a281d47e.jpg?v=0c91e9dc",
     gameUrl: "https://playorca.mobi/"
   },
   {
     id: "playbdd",
     name: "Play BDD",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/play-bdd-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/fcf9c9dd.jpg?v=0c91e9dc",
     gameUrl: "https://www.playbdd.com/"
   },
   {
     id: "kraken",
     name: "The Kraken",
-    imageUrl: "https://sweepshub.us/wp-content/uploads/2024/01/the-kraken-logo.png",
+    imageUrl: "https://shawn-sweepstakes.carrd.co/assets/images/gallery01/f6ee4ae8.jpg?v=0c91e9dc",
     gameUrl: "https://getthekraken.com/"
   }
 ];
